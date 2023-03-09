a25f8e9c582127d0d69894ad0d1d3d4efe274e58
ilkeroz17 <110835938+ilkeroz17@users.noreply.github.com>
2023-03-10 01:41:33 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -40,7 +40,7 @@ if [ -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox_defaults.go -a -f $FOX_MA
     export OF_PATCH_VBMETA_FLAG=1
 
     # Magisk
-    export FOX_USE_SPECIFIC_MAGISK_ZIP="$FOX_MANIFEST_ROOT/device/xiaomi/rubens/Magisk/Magisk-v25.2.zip"
+    export FOX_USE_SPECIFIC_MAGISK_ZIP="$FOX_MANIFEST_ROOT/device/xiaomi/plato/Magisk/Magisk-v25.2.zip"
 
     # Binaries
     export FOX_USE_BASH_SHELL=1
